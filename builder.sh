6fed53524d96a3c497e1f3654c4e81b279bcc063
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-01-19 14:00:56 +0000

    Add files via upload

--- builder.sh
+++ builder.sh
@@ -21,8 +21,27 @@ cp isolinux.cfg /tmp/root 2> /dev/null
 cp vmlinuz /tmp/root 2> /dev/null
 cp initrd.img /tmp/root 2> /dev/null
 cp ldlinux.c32 /tmp/root 2> /dev/null
-
-chmod 777 /tmp/root/*
+cp init /tmp/root 2> /dev/null
+cp linuxrc /tmp/root 2> /dev/null
+mkdir /tmp/root/etc 2> /dev/null
+mkdir /tmp/root/dev 2> /dev/null
+mkdir /tmp/root/bin 2> /dev/null
+mkdir /tmp/root/sbin 2> /dev/null
+mkdir /tmp/root/usr 2> /dev/null
+mkdir /tmp/root/root 2> /dev/null
+mkdir /tmp/root/root/etc 2> /dev/null
+mkdir /tmp/root/root/dev 2> /dev/null
+mkdir /tmp/root//root/bin 2> /dev/null
+mkdir /tmp/root/root/sbin 2> /dev/null
+mkdir /tmp/root/root/usr 2> /dev/null
+mkdir /tmp/root/root/run 2> /dev/null
+mkdir /tmp/root/root/proc 2> /dev/null
+mkdir /tmp/root/root/sys 2> /dev/null
+mkdir /tmp/root/proc 2> /dev/null
+mkdir /tmp/root/sys 2> /dev/null
+mkdir /tmp/root/boot 2> /dev/null
+mkdir /tmp/root/run 2> /dev/null
+chmod 777 /tmp/root/* 2> /dev/null
 
 genisoimage -o "$h" -input-charset utf-8 -b "isolinux.bin" -no-emul-boot -boot-load-size 4  -boot-info-table "/tmp/root"
 chmod 777 $h
